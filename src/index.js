17012051cf9e1fa1e2f78f184cf150322f537483
Martin VF <mvilasanchezf@alumnos.nebrija.es>
2023-01-21 15:19:29 +0100

    Conexión establecida con el canal de Twitch

--- src/index.js
+++ src/index.js
@@ -1,50 +1,39 @@
 const tmi = require('tmi.js');
 
 // Define configuration options
-const opts = {
+const options = {
+  options: {
+    debug: true
+  },
   identity: {
-    username: '<BOT_USERNAME>',
-    password: '<OAUTH_TOKEN>'
+    username: 'merekebot',
+    password: 'oauth:k08s31kdz0b3qu55rnhju9svwborlj'
   },
   channels: [
-    '<CHANNEL_NAME>'
+    'merekebot'
   ]
 };
+const client = new tmi.client(options);
 
-// Create a client with our options
-const client = new tmi.client(opts);
-
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
-// Connect to Twitch:
 client.connect();
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+client.on('connected', (address, port) => {
+  client.action('merekebot', `Hello, connected to ${address}:${port}`);
+});
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+client.on('chat', (target, ctx, message, self) => {
+  // Don't listen to my own messages..
+  if (self) return;
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+  // Do your stuff.
+  console.log(target);
+  console.log(ctx);
+
+  const comandName = message.trim().toLowerCase();
+  if (comandName === '!hello') {
+    client.say(target, `Hello ${ctx.username}!`);
   }
-}
 
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
+  // Send a "chat" message to the target channel
+  client.say(target, `Hello ${ctx.username}, I see you said: ${message}`);
+});
